f62464797561dff23d0553b4b415647676369889
Anhelina Priadko <anhelina.pryadko@gmail.com>
2026-03-27 23:16:49 +0200

    add ExceptionHandlingMiddleware, ApiResponse, DependencyInjection

---
+++ QualityDepartment.Core/DTOs/Common/ApiResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QualityDepartment.Core.DTOs.Common
+{
+    public class ApiResponse<T>
+    {
+        public bool Success { get; set; }
+        public T? Data { get; set; }
+        public string? Message { get; set; }
+        public List<string>? Errors { get; set; }
+
+        public static ApiResponse<T> SuccessResponse(T data, string? message = null)
+            => new() { Success = true, Data = data, Message = message };
+
+        public static ApiResponse<T> FailureResponse(List<string> errors, string? message = null)
+            => new() { Success = false, Errors = errors, Message = message };
+    }
+}
