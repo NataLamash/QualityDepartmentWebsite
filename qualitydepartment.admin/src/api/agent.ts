2cc7c17825ae27f5bf38f4976b14dd1bf725b605
Sasha25046 <161481283+Sasha25046@users.noreply.github.com>
2026-05-08 12:00:00 +0300

    NewsAdmin

--- qualitydepartment.admin/src/api/agent.ts
+++ qualitydepartment.admin/src/api/agent.ts
@@ -12,27 +12,37 @@ const requests = {
 const agent = {
     Auth: {
         login: (body: { email: string; password: string }) =>
-            requests.post('/auth/login', body),
+            requests.post<any>('/auth/login', body),
     },
-
     News: {
-        list: () => requests.get('/admin/news'),
+     list: (page = 1, pageSize = 50) => 
+        requests.get<any>(`/admin/news?pageNumber=${page}&pageSize=${pageSize}`),
+    
+     create: (news: FormData) => requests.post<void>('/admin/news', news),
+     update: (id: number, news: FormData) => requests.put<void>(`/admin/news/${id}`, news),
+    
+     delete: (id: number) => requests.del<void>(`/admin/news/${id}`),
     },
 
     Documents: {
-        list: () => requests.get('/admin/documents'),
+        list: () => requests.get<any>('/admin/documents'),
+        create: (data: FormData) => requests.post<void>('/admin/documents', data),
+        delete: (id: number) => requests.del<void>(`/admin/documents/${id}`),
     },
 
     AdministrationMembers: {
-        list: () => requests.get('/admin/administration-members'),
+        list: () => requests.get<any>('/admin/administration-members'),
+        create: (data: FormData) => requests.post<void>('/admin/administration-members', data),
+        update: (id: number, data: FormData) => baseApi.put(`/admin/administration-members/${id}`, data).then(responseBody),
+        delete: (id: number) => requests.del<void>(`/admin/administration-members/${id}`),
     },
 
     UsefulInformation: {
-        list: () => requests.get('/admin/external-links'),
+        list: () => requests.get<any>('/admin/external-links'),
     },
 
     Categories: {
-        list: () => requests.get('/admin/document-categories'),
+        list: () => requests.get<any>('/admin/document-categories'),
     },
 };
 
